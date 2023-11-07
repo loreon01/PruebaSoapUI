475b77e49454c7dfa5bbe544412279dca6f861e0
ET24DE17 <PC-1@PC-GAB1-07>
2023-11-07 11:22:58 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PruebaSoapUI.Soap1;
+
+namespace PruebaSoapUI
+{
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            Soap1.TipoCambio cliente = new Soap1.TipoCambio();
+
+            var respuesta = cliente.TipoCambioDia();
+            Console.WriteLine(respuesta.CambioDolar.First().fecha);
+            Console.WriteLine(respuesta.CambioDolar.First().referencia);
+            Console.Read();
+
+
+
+
+
+
+
+        }
+    }
+}
